9861bcc779ef179ac23b2fafb80a6ff4d385822a
Sudam Mahagamage <sudamtm@gmail.com>
2021-08-01 02:20:56 +0530

    completed python tasks

---
+++ task-8/hash-script.py
@@ -0,0 +1,16 @@
+#Following script doesnt work on macOS (also >python3.3) due to
+#the included crypt package in macos being different,
+#i cant afford a mac so its not my problem -Sudam
+
+#(or use passlib - platform independant package)
+
+import sys
+import crypt
+
+#Config
+password=sys.argv[1]
+hashedValue=crypt.crypt(password, crypt.mksalt(crypt.METHOD_SHA512))
+
+#Output
+print('Value to be hashed :',sys.argv[1])
+print('Hashed Value :',hashedValue)
